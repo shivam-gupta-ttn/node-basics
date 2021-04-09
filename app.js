b16cf6e0766e911c2dc5703b2e5d4c34c124d16f
shivam-gupta-ttn <shivam.gupta@tothenew.com>
2021-04-09 16:24:20 +0530

    nodeJS basics

---
+++ app.js
@@ -0,0 +1,14 @@
+// const fs = require('fs');
+
+
+// fs.readFile('text', function(err,data){
+//     if(err){
+//         return console.log(err)
+//     }
+//     console.log(data.toString())
+
+// });
+// console.log("reached here")
+
+var demoModule = require("./demo-module");
+demoModule.myFun();
